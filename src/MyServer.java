f0cbdcd2c49dc21c8ae6ef16289e64224e979b74
Devotioe <71645345+Devotioe@users.noreply.github.com>
2026-05-30 14:55:28 +0800

    add : My own client server

---
+++ src/MyServer.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.*;
+
+
+public class MyServer {
+	private static final String HOST = "127.0.0.1";
+	private static final int PORT = 9527;
+	
+	public static void main(String[] args) {
+		
+		ServerSocket serverSocket = null;
+		Socket clientSocket = null;
+		
+		try{
+			serverSocket = new ServerSocket();
+			System.out.println("Waiting for client to connect...");
+			
+			serverSocket.bind(new InetSocketAddress(HOST, PORT)); //bind process to IP address, enabling client to send package
+			System.out.println("Server binded to " + HOST + ":" + PORT);
+			
+			//while(true) {
+			clientSocket = serverSocket.accept(); //blocking method ; waiting for client to connect
+			System.out.println("Client connected to server");
+			//}
+			
+			BufferedReader clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //buffer reader to get 
+			String clientInput = clientReader.readLine(); //blocking method ; wait for buffer to have content to read
+			
+			System.out.println("Client sent: " + clientInput);
+			
+			
+			PrintWriter msgSender = new PrintWriter(clientSocket.getOutputStream(), true); //
+			msgSender.println(clientInput);
+			System.out.println("Echoing back to the client...");
+			
+			
+			serverSocket.close();
+			System.out.println("Server closing...");
+			
+			
+			
+			
+		}catch(Exception e){
+			
+		}
+		
+		
+		
+	}
+}
--- src/Server.java
+++ src/Server.java
@@ -1,41 +1,68 @@
-import java.io.*;
-import java.net.*;
 
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/* create a EchoServer class*/
 public class Server {
-	
-	protected static final int PORT = 5987; 
-	
-    public static void main(String[] args) throws IOException {
- 
-    	ServerSocket serverSocket = null;
-    	//BufferedReader buf = new BufferedReader();
-    	
-    	
-    	
-        try {
-			serverSocket = new ServerSocket(PORT);
-			System.out.println("Server listening on PORT " + PORT);
+
+	/* socket parameters */
+	private static final int PORT = 9527;
+	private static final String HOST = "127.0.0.1";
+
+	public static void main(String[] args) {
+		
+		try {
+		
+			System.out.println("Waiting for clients...");
 			
-			while(true) {
-				 Socket clientSocket = serverSocket.accept(); //blocking method, only continue when accepted client's socket
-				 System.out.println("Client connected to server");
-				 
-				 
-				 
+			/* Create a serversocket instance. */
+			ServerSocket serverSocket = new ServerSocket();
+			
+			/* Let the OS bind the created ServerSocket by using user-specified parameters. */
+			/* The 1st parameter is the binded IP, and the 2nd parameter is the binded port number. */
+			/* In java, there is no need to call listen(). */
+			serverSocket.bind(new InetSocketAddress(HOST, PORT));
+			System.out.println("Server binded to " + HOST + ":" + PORT);
+			
+			/* Sever waits for accepting a request from a client. */
+			Socket clientSocket = serverSocket.accept();
+			System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+			
+			/* InputStreamReader gets the input byte stream from the socket, */
+			/* and store the data in a BufferReader for latter reading. */
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));			
+			/* The program could read a line from the socket buffer. */
+			String str = in.readLine();
+			
+			/* You can implement whatever you want after receiving a message from the client. */
+			/* In this example, we echo the message back to the client. */
+			if(str != null) {
+				/* Show the message sent from the client. */
+				System.out.println("Received from client " + clientSocket.getInetAddress().getHostAddress() + ": " + str);
+				
+				/* In order to send message back to the client,
+				/* we setup a Printwriter and connect it to the clientSocket's getOutputStream().
+				/* The second parameter of PrintWriter is 'autoFlush'. */
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				/* Now, we send some string back to the client via the socket. */
+				out.println("Server responsed: " + str);
+				System.out.println("Server echoed back to client " + clientSocket.getInetAddress().getHostAddress() + ": " + str);
 			}
-			
-			
-			
-			} catch (IOException e) {
-			
-			e.printStackTrace();
+		} 
+		/* Exception handeling. */
+		catch (IOException e) {
+			System.err.println("I/O error: " + e.getMessage());
 		}
-        
-        
-        
-        
-       
-        
-    
-    }
+		catch (Exception e) {
+			System.err.println("Server error: " + e.getMessage());
+		} 
+		finally {
+			System.out.println("Server is closing...");
+		}
+
+	}
+
 }
